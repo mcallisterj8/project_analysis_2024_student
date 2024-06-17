9a3bb5a3fccc3d79e6f091932c9092367dcf20c6
Jacob McAllister <mcallisterj@westerntc.edu>
2024-06-17 15:47:22 -0500

    End of lecture 6-17-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- node_apps/product-store/routes/products.js
+++ node_apps/product-store/routes/products.js
@@ -3,7 +3,31 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
+let products = [];
+const productsFile = "./database/products.json";
 
+fs.readFile(productsFile, (err, data) => {
+    if(err) {
+        throw err;
+    }
 
+    products = JSON.parse(data);
+});
+
+router.get("/", (req, res) => {    
+    res.json(products);
+});
+
+router.get("/hello/again", (req, res) => {
+    res.json({hello: "world again!"});
+});
+
+router.get("/hello/:id", (req, res) => {
+    res.json({hello: "world with id: " + req.params.id});
+});
+
+router.get("/hello", (req, res) => {
+    res.json({hello: "world"});
+});
 
 module.exports = router;
