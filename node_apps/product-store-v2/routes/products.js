55013d03e3f0a3cce61a2c29bb5fa49d24473346
Jacob McAllister <mcallisterj@westerntc.edu>
2024-07-01 15:24:45 -0500

    Class update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- node_apps/product-store-v2/routes/products.js
+++ node_apps/product-store-v2/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Op } = require("sequelize");
 const Product = require("../models/product");
 
-// GET all products
+// GET all products /products/
 router.get("", async (req, res) => {
     // get all products
     const products = await Product.findAll();
@@ -11,15 +11,56 @@ router.get("", async (req, res) => {
     res.json(products);
 });
 
-router.get("/:id", async (req, res) => {
-    const product = await Product.findByPk(req.params.id);
+// GET /products/search
+router.get("/search", async (req, res) => {    
+    const name = req.query.name;
+    // const { name } = req.query;
 
+    if(!name) {
+        return res.status(400).json({error: "Invalid search."});
+    }
+
+    const products = await Product.findAll({
+        where: {
+            name: {
+                [Op.like]: `%${name}%`,
+            },           
+        },
+    });
+
+    res.json(products);
+     
+});
+
+// GET /products/2
+router.get("/:id", async (req, res) => { 
+    console.log("\n\n");
+    console.log(req.params);
+    console.log("\n\n");   
+    const product = await Product.findByPk(req.params.id);
+    
     if(product) {
-        res.json(product);
+        res.json(product);        
     } else {
         res.status(404).json({error: "Product not found."});
     }
 
 });
+
+router.post("", async (req, res) => {
+    const { name, price } = req.body;
+    
+    if(!name || !price) {
+        return res.status(400).json({ error: "Name and price are required" });
+    }
+
+    const newProduct = await Product.create({name, price});
+
+    res.status(201).json({
+        success: true,
+        message: "Product added",
+        newProduct
+    });
+})
 
 module.exports = router;
